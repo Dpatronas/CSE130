9b6b5804a2c2957ffd6e1841718bd8266a5a01dd
Despina <dpatrona@ucsc.edu>
2021-10-06 21:54:38 -0700

    change errx msg, added stdin buffer

--- asgn0/shoulders.c
+++ asgn0/shoulders.c
@@ -17,7 +17,7 @@ int const SIZE = 4096;
 // warn usage - https://linux.die.net/man/3/warn
 
 // Returns 1 when chars of string are digits, else returns 0
-int isDigit(char* num){
+int isDigit(char* num) {
   int len = strlen(num);
   char *n = num;
 
@@ -28,14 +28,34 @@ int isDigit(char* num){
   return 1;
 }
 
+// Used to populate buffer and write to fd
+// fd = file or stdout
+void Buffer(int lines, int fd) {
+  char buff[SIZE];        // buffer to hold file contents
+  int read_bytes = 0;     // bytes read
+  int count = 0;          // keeps track of characters for buffer
+
+  //loop through lines                   
+  for (int j = 0; j < lines; j++) {
+    //loop through characters of line
+    do {
+      char ch = 0;
+      read_bytes = read(fd, &ch, 1);     // read 1 char at a time
+      buff[count] = ch;                  // populate buffer
+      if(buff[count] == '\n')            // exit line for \n char
+        break;
+      count++;                           // otherwise keep looping        
+    }
+    while (read_bytes != 0);             // EOF break
+    write(STDOUT_FILENO, buff, count+1); // write line buffer to stdout
+    count = 0; 
+    memset( buff, 0, SIZE);              // reset for next line or file
+  }
+}
 
 int main(int argc, char** argv) {
-  int lines;                    // argv[1]
-  int fd;                       // file descriptor
-  char buff[SIZE];       // buffer to hold file contents
-  int read_bytes = 0;           // bytes read
-  int count = 0;                // keeps track of characters for buffer
-
+  int lines;              // argv[1]
+  int fd;                 // file descriptor
   //no CLI args
   if(argc == 1) 
     fprintf(stderr, "shoulders: requires an argument");
@@ -43,42 +63,37 @@ int main(int argc, char** argv) {
   //Loop through CLI args
   for (int i = 0; i < argc; i++) {
 
-    //Lines arg
+    //Lines CLI arg
     if(i==1) {
       if(strncmp(argv[1],"-qn", 3)==0)                
         fprintf(stderr,"shoulders: option requires an argument -- 'n' \nTry ‘shoulders --help' for more information.");
 
       else if(atoi(argv[1])<0 || !isDigit(argv[1])) {
-        fprintf(stderr, "shoulders: invalid number of lines: '%s'", argv[1]);
+        errx(1, "invalid number of lines: '%s'", argv[1]);
       }
       lines = atoi(argv[1]);
+
+      // no file args
+      if (argc == 2) {
+        Buffer(lines, STDIN_FILENO);
+      }
     }
 
-    // No files in arg || file has '-'
-    // In Progress
-
-    // Files args
+    // Files CLI args
     if(i>=2) {
-      if ((fd = open(argv[i], O_RDONLY, 0)) < 0)  // open file for arg             
-          err(1, "%s", argv[0]);                  // bad file open
-      
-      //loop through files lines                   
-      for (int j = 0; j < lines; j++) {
-        //loop through characters of line
-        do {
-          char ch = 0;
-          read_bytes = read(fd, &ch, 1);     // read 1 char at a time
-          buff[count] = ch;                  // populate buffer
-          if(buff[count] == '\n')            // exit line for \n char
-            break;
-          count++;                           // otherwise keep looping        
-        }
-        while (read_bytes != 0);             // EOF break
 
-        write(STDOUT_FILENO, buff, count+1); // write line buffer to stdout
-        count = 0; 
-        memset( buff, 0, SIZE);              // reset for next line or file
+      // -file
+      if (strncmp(argv[i], "-", 1) ==0) {
+        Buffer(lines, STDIN_FILENO);
+        continue;
       }
+
+      // open file
+      if ((fd = open(argv[i], O_RDONLY, 0)) < 0) { // open file for arg             
+        warn("cannot open '%s'", argv[i]);         // bad file open
+        continue;
+      }
+      Buffer(lines, fd);
     }
   }
   return 0;
