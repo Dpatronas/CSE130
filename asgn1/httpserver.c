ca3b0fb4246d4efaa1e54384d77e69aaf433af71
Despina <dpatrona@ucsc.edu>
2021-11-02 21:04:59 -0700

    final revision before multithreading

--- asgn1/httpserver.c
+++ asgn1/httpserver.c
@@ -1,13 +1,16 @@
-#include <err.h>
-#include <stdint.h>
+// Despina Patronas CSE130 Fall 2021
+
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <stdio.h>      // fprintf(stderr, ...), scanf()
-#include <errno.h>      // check warnings
+#include <stdint.h>
+#include <string.h>     // strtok(), strstr(), strncpy(), strlen()
+#include <stdio.h>      // fprintf(), scanf()
+#include <err.h>        // warn()
+#include <errno.h>      // check warn codes
 #include <fcntl.h>      // open()
-#include <sys/stat.h>   // fstat
-#include <ctype.h>
+#include <unistd.h>     // close()
+#include <ctype.h>      // isalnum()
+#include <sys/stat.h>   // stat()
+
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
@@ -15,18 +18,19 @@
 
 #define HEADER_SIZE       1024
 #define PROCESS_BODY_SIZE 4096
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
 extern int errno;
 
-// Methods
+// Commands server supports
 typedef enum method {
   _GET_ = 0,
   _PUT_,
   _HEAD_,
 } method;
 
+// Message object
 struct ClientRequest {
-
   int socket;           // Client connection fd
   int method;
   char resource [300];
@@ -36,7 +40,7 @@ struct ClientRequest {
 
 } ClientRequest;
 
-// status code options
+// Codes server supports
 const char* Status(int code) {
   switch(code) {
     case 200: return "OK";                    // Response is successful ie: nothing else broke
@@ -44,16 +48,15 @@ const char* Status(int code) {
     case 400: return "Bad Request";           // Request is not valid ie: not parsable
     case 403: return "Forbidden";             // Cannot access valid resource file (for GET)
     case 404: return "File Not Found";        // Valid resource name but server cannot find file
-    case 500: return "Internal Server Error"; // Request is valid but cannot allocate memory to process request
+    case 500: return "Internal Server Error"; // Request is valid but cant allocate memory to process
     case 501: return "Not Implemented";       // Request is valid is ok BUT command not valid
   }
   return NULL;
 }
 
-/**
-   Converts a string to an 16 bits unsigned integer.
-   Returns 0 if the string is malformed or out of the range.
- */
+
+// Converts a string to an 16 bits unsigned integer.
+// Returns 0 if the string is malformed or out of the range.
 uint16_t strtouint16(char number[]) {
   char *last;
   long num = strtol(number, &last, 10);
@@ -63,10 +66,10 @@ uint16_t strtouint16(char number[]) {
   return num;
 }
 
-/**
-   Creates a socket for listening for connections.
-   Closes the program and prints an error message on error.
- */
+
+
+// Creates a socket for listening for connections.
+// Closes the program and prints an error message on error.
 int create_listen_socket(uint16_t port) {
   struct sockaddr_in addr;
   int listenfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -82,6 +85,8 @@ int create_listen_socket(uint16_t port) {
   return listenfd;
 }
 
+
+
 // Send client response for request
 void ServerResponse(int code, struct ClientRequest rObj) {
 
@@ -94,6 +99,7 @@ void ServerResponse(int code, struct ClientRequest rObj) {
       code, Status(code), slen, Status(code));
     send(rObj.socket, response, strlen(response), 0); 
   }
+
   else { // GET
     sprintf(response, "HTTP/1.1 %d %s\r\nContent-Length: %d\r\n\r\n", 
       code, Status(code), rObj.len);
@@ -103,48 +109,54 @@ void ServerResponse(int code, struct ClientRequest rObj) {
 }
 
 
-/**
- * Returns 1 if request field is bad
-*/
+
+// Returns 1 if request field is bad
 int isBadRequest(struct ClientRequest * rObj) {
 
-  // Bad Version
+  // Check version matches protocol
   if (!(strncmp(rObj->hostname,"HTTP/1.1",8) == 0)) {
     return 1;
   }
-  // File name checks
+
+  // Check file name begins with backslash
   if (!(strncmp(rObj->resource, "//", 1) == 0)) {
     return 1;
   }
-  //remove file backslash
-  memmove(rObj->resource, rObj->resource+1, strlen(rObj->resource));  
 
-  if (strlen(rObj->resource) > 19) {
+  // Remove file name backslash
+  memmove(rObj->resource, rObj->resource+1, strlen(rObj->resource));  
+  
+  // Check file length
+  if (strlen(rObj->resource) > 19) {  
     return 1;
   }
+
+  // Check file name characters is valid
   for (size_t i = 0; i < strlen(rObj->resource); i++) {
     if ( !(isalnum(rObj->resource[i])) && (rObj->resource[i] != '_') && (rObj->resource[i] != '.')) {
       return 1;
     }
   }
-  // Check host value
-  for (size_t i = 0; i < strlen(rObj->hostvalue); i++) {
-    if (isspace(rObj->hostvalue[i])) {
-      return 1;
-    }
-  }
+
   return 0;
 }
 
 
-void processBodyPUT(int connfd, unsigned int len, int outfile) {
+
+// Process body for PUT and GET requests
+void processBody(int infile, unsigned int len, int outfile) {
   int tot_read = 0;
 
   char* readbuff = (char *)malloc(PROCESS_BODY_SIZE);
   if(!readbuff) { fprintf(stderr, "Bad malloc!"); return; }
 
- while(1){
-    int rdCount = read(connfd, readbuff, PROCESS_BODY_SIZE);
+ while(1)
+ {
+    int remainingBytes = len - tot_read;
+    int RDSIZE = MIN(PROCESS_BODY_SIZE, remainingBytes);  // dont read over required length
+
+    int rdCount = read(infile, readbuff, PROCESS_BODY_SIZE);
+
     tot_read += rdCount;
 
     write(outfile, readbuff, rdCount);
@@ -158,72 +170,48 @@ void processBodyPUT(int connfd, unsigned int len, int outfile) {
 }
 
 
-void processBodyGET(int infile, unsigned int len, int connfd) {
-  int tot_read = 0;
 
-  char* readbuff = (char *)malloc(PROCESS_BODY_SIZE);
-  if(!readbuff) { fprintf(stderr, "Bad malloc!"); return; }
-
- while(1){
-    int rdCount = read(infile, readbuff, PROCESS_BODY_SIZE);
-    tot_read += rdCount;
-
-    write(connfd, readbuff, rdCount);
-
-    if(tot_read >= len) {
-      break;
-    }
-  }
-  free(readbuff);
-}
-
-
-/**
- * Parses Put
- *  Checks whether to create or truncate existing file
-*/
+// Parses Put request pre operation
+//    - Checks whether to create or truncate file requested
 int ParsePut(struct ClientRequest * rObj) {
 
-  int code = 0; // return value
-
-  if (access(rObj->resource, F_OK) == 0) {
+  int code = 0;
+  if (access(rObj->resource, F_OK) == 0)
     code = 200;  // truncate code OK
-  }
-  else {
-    code = 201;  // create code CREATED
-  }
 
-  int outfile = open(rObj->resource, O_RDWR | O_CREAT | O_TRUNC, 0622); // PUT resource file
+  else
+    code = 201;  // create code CREATED
+
+  int outfile = open(rObj->resource, O_RDWR | O_CREAT | O_TRUNC, 0622); 
   if (outfile < 0) { 
     warn("creating outfile error"); 
-    return -1; 
+    return 500; // making resource failed.. error occured..
   }
   
-  // empty file contents nothing to process
+  // Empty header body -> nothing to process
   if (rObj->len == 0) {
     close(outfile);
     return code;
   }
 
-  // write recv'd bytes into file resource
-  processBodyPUT(rObj->socket, rObj->len, outfile);
+  // Write bytes from socket body into file resource
+  processBody(rObj->socket, rObj->len, outfile);
   close(outfile);
   return code;
 }
 
 
-/**
- * Parses GET or HEAD. 
- * note: Head does not process bytes.
- *  Checks whether file resources exists and/or is accessible.
-*/
+
+// Parses GET or HEAD pre operation
+//    - HEAD does not process any bytes.
+//    - GET  checks whether file resources exists and/or is accessible.
 void ParseGetHead(struct ClientRequest * rObj) {
   int infile;
-  // Check errors
-  if ((infile = open(rObj->resource, O_RDONLY, 0)) < 0) {              // open file
-    warn("cannot open '%s' due to ernno: %d", rObj->resource, errno);  // bad file open
+  // Attempt to open file requested
+  if ((infile = open(rObj->resource, O_RDONLY, 0)) < 0) {
+    warn("cannot open '%s' due to ernno: %d", rObj->resource, errno);
 
-    if (errno == 2) {                                     // check errno for status code
+    if (errno == 2) {
       ServerResponse(404, *rObj); // DNE
     }
     else {
@@ -240,28 +228,25 @@ void ParseGetHead(struct ClientRequest * rObj) {
   ServerResponse(200, *rObj);
 
   if (rObj->method == _GET_) {
-    processBodyGET(infile, rObj->len, rObj->socket);
+    processBody(infile, rObj->len, rObj->socket);
   }
   close(infile);
 }
 
 
-// Parse lines of header
-void ParseLine(char* input, struct ClientRequest * rObj) {
+
+// Parse line of request header
+//    - Populate the message object fields
+//    - returns -1 if header fields not accepted.
+//    - ignored extraneous lines..
+int ParseLine(char* line, struct ClientRequest * rObj) {
   int param_count = 0;
   int param_type = -1;
 
   char params[5][HEADER_SIZE];
   // Lines expected
   enum {
-    _GET = 0,
-    _PUT,
-    _HEAD,
-    _HOST,
-    _USER_AGENT,
-    _ACCEPT,
-    _CONTENT_LENGTH,
-    _EXPECT,
+    _GET = 0, _PUT, _HEAD, _HOST, _USER_AGENT, _ACCEPT, _CONTENT_LENGTH, _EXPECT,
 
     PARAM_TOTAL,
   };
@@ -280,12 +265,12 @@ void ParseLine(char* input, struct ClientRequest * rObj) {
   };
   char * tok = NULL;
 
-  tok = strtok (input, " ");
+  tok = strtok (line, " ");
   while (tok != NULL) 
   {
     if (param_count == 0) {
       for (int i = 0; i < type_count; ++i) {
-        if (strstr(tok, types[i])) {  // search for token in the 
+        if (strstr(tok, types[i])) {
           param_type = i;
           break;
         }
@@ -298,72 +283,108 @@ void ParseLine(char* input, struct ClientRequest * rObj) {
     tok = strtok (NULL, " ");
   }
 
+  // Unexpected line
   if (param_type == -1) {
-    printf("Unknown req/param, ignoring line\n");
-    return;
+    fprintf(stderr, "Unknown req/param, ignoring line\n");
+    return -1;
   }
+
   switch(param_type) {
-    case _GET: {
+    case _GET: {  // todo error check that param_count is within range of expectation..
       rObj->method = _GET_;
-      strcpy(rObj->resource, params[0]); strcpy(rObj->hostname, params[1]);
+      strcpy(rObj->resource, params[0]); 
+      strcpy(rObj->hostname, params[1]);
       break;
     }
+
    case _PUT: {
       rObj->method = _PUT_;
-      strcpy(rObj->resource, params[0]); strcpy(rObj->hostname, params[1]);
+      strcpy(rObj->resource, params[0]); 
+      strcpy(rObj->hostname, params[1]);
       break;
    }
+
   case _HEAD: {
       rObj->method = _HEAD_;
-      strcpy(rObj->resource, params[0]); strcpy(rObj->hostname, params[1]);
+      strcpy(rObj->resource, params[0]); 
+      strcpy(rObj->hostname, params[1]);
       break;
    }
+
    case _HOST: {
-      tok = strtok(params[0], ":");     
+      //Additional parameter(s) than expected indicate spaces on hostvalue
+      if (param_count > 2) { 
+        return -1;
+      }
+      // Grab host value from hostname
+      tok = strtok(params[0], ":"); 
       tok = strtok (NULL, " ");
       strcpy(rObj->hostvalue, tok);
       break;
    }
+
    case _USER_AGENT: {
       break;
    }
+
    case _ACCEPT: {
       break;
    }
+
+   // Assume length is a valid number generated by client curl
    case _CONTENT_LENGTH: {
       rObj->len = atoi(params[0]);     
       break;
    }
+
    case _EXPECT: {
       break;
    }
+
    default: {
-      printf("Unknown param type: %i\n", param_type);
+      fprintf(stderr, "Unknown param type: %i\n", param_type);
       break;
     }
   }
+  return 1;
 }
 
-// Parse header line by line
-void ParseHeader(char * request, struct ClientRequest * rObj) {
+
+
+// Parse request header (line by line)
+// Returns -1 if header was not processed due to bad header request.
+int ParseHeader(char * request, struct ClientRequest * rObj) {
  char * tok = strtok (request, "\r\n");
  while (tok != NULL)
  {
-   request += strlen(tok) + strlen("\r\n");
-   ParseLine(tok, rObj);
-   tok = strtok (request, "\r\n");
+   request += strlen(tok) + strlen("\r\n"); // manually set index
+   int parse_status = ParseLine(tok, rObj);      // return status of parseLine
+   if (parse_status < 0) {
+      return -1;
+   }
+   tok = strtok (request, "\r\n");  // get next line
  }
+ return 1;
 }
 
 
+
+// Deconstruct request by parts
+//    - parse header
+//    - check bad request line fields are satisfactory
+//    - attempt to fulfill request HEAD, GET, PUT
 void ProcessRequest(char *request, int connfd) {
 
   struct ClientRequest rObj = {0}; //reset object
   rObj.socket = connfd;
 
-  ParseHeader(request, &rObj);
+  int parse_status = ParseHeader(request, &rObj);
+  if (parse_status < 0) {
+    ServerResponse(400, rObj);
+    return;
+  }
 
-  // Check Request
+  // Check request fields satisfy requirements
   if (isBadRequest(&rObj)) {
     ServerResponse(400, rObj);
     return;
@@ -388,10 +409,10 @@ void ProcessRequest(char *request, int connfd) {
 }
 
 
+
+// Receive client requests
 void HandleConnection(int connfd) {
-
   char request[HEADER_SIZE];   
-
   while(1) 
   {
     memset(request,0,sizeof request);
@@ -404,6 +425,8 @@ void HandleConnection(int connfd) {
 }
 
 
+
+// Grab clients
 int main(int argc, char *argv[]) {
   int listenfd;
   uint16_t port;
